3b81db5e7d53f66aae8e55b5606e8fad01d970a0
Scott Garner <scott@j38.net>
2012-11-17 16:24:18 -0500

    First working version

--- beetboard.py
+++ beetboard.py
@@ -1,14 +1,23 @@
 import pygame
-from pygame.locals import *
-import time
-import curses
 
-#import mpr121
+import RPi.GPIO as GPIO
+import mpr121
+
+# Use GPIO Interrupt Pin
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(7, GPIO.IN)
+
+# Use mpr121 class for everything else
+
+mpr121.TOU_THRESH = 0x22
+mpr121.REL_THRESH = 0x26
+mpr121.setup(0x5a)
+
+# User pygame for sounds
 
 pygame.mixer.pre_init(44100, -16, 12, 512)
 pygame.init()
-
-#mpr121.setup(0x5a)
 
 kick = pygame.mixer.Sound('kick.wav')
 snare = pygame.mixer.Sound('snare.wav')
@@ -17,44 +26,44 @@ closedhh = pygame.mixer.Sound('closed.wav')
 clap = pygame.mixer.Sound('clap.wav')
 cymbal = pygame.mixer.Sound('cymbal.wav')
 
-screen = curses.initscr()
-curses.noecho()
-curses.curs_set(0)
-screen.keypad(1)
+# Track touches
 
-screen.clear()
+touches = [0,0,0,0,0,0];
 
 while True:
 
-	#touchData = mpr121.readData(0x5a)
-
-	#for i in range(6):
-	#	touchValue = touchData & (1<<i)
-	#	screen.addstr(i+1, 0, str(touchValue))
-
-	#screen.addstr(0, 0, str(touchData))
-
-	event = screen.getch()
-
-	screen.clear()
-
-	if event == ord('q'):
-		break
-	elif event == ord('1'):	
-		kick.play();
-	elif event == ord('2'):	
-		snare.play();
-	elif event == ord('3'):	
-		openhh.play();
-	elif event == ord('4'):	
-		closedhh.play();
-	elif event == ord('5'):	
-		clap.play();
-	elif event == ord('6'):	
-		cymbal.play();										
+	if (GPIO.input(7)):
+		pass
 	else:
-		screen.clear()
-		screen.addstr(0, 0, "That key doesn't do anything!")
+		touchData = mpr121.readData(0x5a)
 
+		print bin(touchData)
 
-curses.endwin()
+		for i in range(6):
+			if (touchData & (1<<i)):
+
+				if (touches[i] == 0):
+
+					print( 'Pin ' + str(i) + ' was just touched')
+
+					if (i == 0):
+						kick.play()
+					elif (i == 1):
+						snare.play()
+					elif (i == 2):
+						openhh.play()
+					elif (i == 3):
+						closedhh.play()
+					elif (i == 4):
+						clap.play()
+					elif (i == 5):
+						cymbal.play()
+
+				touches[i] = 1;
+			else:
+				if (touches[i] == 1):
+					pass
+					#print( 'Pin ' + str(i) + ' was just released')
+
+				touches[i] = 0;
+
